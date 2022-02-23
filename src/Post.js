9f70a72026a6e38a7c53fefa6ae91b878b8f7ba1
Kim._.jazz <jeong011010@naver.com>
2022-02-23 17:12:20 +0900

    update [make postline]

--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import './Post.css'
 
 function Post() {
   return (
-    <div>
+    <div className="post">
+        <h3>Username</h3>
       {/* header -> avater + username */}
 
+        <img className="post_image" src="https://t1.daumcdn.net/cfile/tistory/9960C8455A901F7E29"/>
       {/* image */}
-
+        <h4 className="post_text"><strong>UserID</strong> caption</h4>
       {/* username + caption */}  
     </div>
   )
