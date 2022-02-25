9aa703371ca7cf3a1aa9ce5f7fcba7d51f536487
Kim._.jazz <jeong011010@naver.com>
2022-02-26 06:01:18 +0900

    update js file [use Modal (Sign up), use Firebase User Authentication]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Post from './Post';
 import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import {Button} from '@material-ui/core';
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2,4,3),
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
+  const [open,setOpen] = useState(false);
 
   useEffect(()=>{
     db.collection('posts').onSnapshot(snapshot => {
@@ -16,8 +46,22 @@ function App() {
     })
   }, []);
 
+  const signUp = (event) => {
+    
+  }
+
   return (
     <div className='App'>
+
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
+
 
       <div className="app_header">
         <img
@@ -26,6 +70,8 @@ function App() {
           alt=""
         />
       </div>
+
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
       <h1>Hi😄</h1>
 
