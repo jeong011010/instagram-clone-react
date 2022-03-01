61a88e1d50531d8bba51f29985378bea093337f0
Kim._.jazz <jeong011010@naver.com>
2022-03-02 00:42:28 +0900

    update folder name

--- src/Post.js
+++ src/Post.js
@@ -3,6 +3,8 @@ import './Post.css'
 import Avatar from '@material-ui/core/Avatar'
 
 function Post({ username, caption, imageUrl }) {
+  const [comments, setComments] = useState([]);
+
   return (
     <div className="post">
       <div className="post_header">
